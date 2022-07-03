217c769532cf4928338a7267e514b2b73186a0c9
Charsh177 <harshchougule177@gmail.com>
2022-07-03 18:30:54 +0530

    ADDED: Welcome to Indian State Analyser Program

---
+++ src/main/java/com/bl/statecensus/StateMain.java
@@ -0,0 +1,9 @@
+package com.bl.statecensus;
+
+public class StateMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Indian States Census Analyser Program");
+    }
+}
